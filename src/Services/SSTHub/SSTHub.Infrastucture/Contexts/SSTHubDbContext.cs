c5839b52a16b0081368e644afbc0a99f02a6c3c3
Roman Sydor <r.sydor@eleks.com>
2024-01-16 15:28:35 +0200

    add setup data

--- src/Services/SSTHub/SSTHub.Infrastucture/Contexts/SSTHubDbContext.cs
+++ src/Services/SSTHub/SSTHub.Infrastucture/Contexts/SSTHubDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SSTHub.Domain.Entities;
+using SSTHub.Domain.Enums;
 using SSTHub.Infrastucture.EntityConfigurations;
 
 namespace SSTHub.Infrastucture.Contexts
@@ -21,6 +22,119 @@ namespace SSTHub.Infrastucture.Contexts
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(HubConfiguration).Assembly);
+
+            modelBuilder.Entity<Hub>().HasData(
+                new Hub
+                {
+                    Id = 1,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    Name= "TestHub1",
+                },
+                new Hub
+                {
+                    Id = 2,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    Name = "TestHub2",
+                }
+            );
+
+            modelBuilder.Entity<Service>().HasData(
+                new Service
+                {
+                    Id = 1,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    Name = "TestService1",
+                    DurationInMinutes = 60,
+                    Price = 100,
+                },
+                new Service
+                {
+                    Id = 2,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    Name = "TestService2",
+                    DurationInMinutes = 60,
+                    Price = 100,
+                }
+            );
+
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer
+                {
+                    Id = 1,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "Roman",
+                    LastName = "Aaaa",
+                    Email = "testRA@test.com",
+                    Phone = "1111111111",
+                },
+                new Customer
+                {
+                    Id = 2,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "Ivan",
+                    LastName = "BBBB",
+                    Email = "testIB@test.com",
+                    Phone = "2222222222",
+                }
+            );
+
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "Dmytro",
+                    LastName = "Watson",
+                    Email = "testDW@test.com",
+                    Phone = "1231231231",
+                    Position = EmployeePositions.Administrator,
+                    HubId = 1,
+                },
+                new Employee
+                {
+                    Id = 2,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    FirstName = "Petro",
+                    LastName = "Abc",
+                    Email = "testPA@test.com",
+                    Phone = "43434343433",
+                    Position = EmployeePositions.Barber,
+                    HubId = 2,
+                }
+            );
+
+            modelBuilder.Entity<Event>().HasData(
+                new Event
+                {
+                    Id = 1,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    StartAt = DateTime.Now.AddDays(1),
+                    Status = EventStatuses.Created,
+                    CustomerId = 1,
+                    EmployeeId = 1,
+                    HubId = 1,
+                    ServiceId = 1,
+                },
+                new Event
+                {
+                    Id = 2,
+                    IsActive = true,
+                    CreatedAt = DateTime.Now,
+                    StartAt = DateTime.Now.AddDays(-1),
+                    Status = EventStatuses.Finished,
+                    CustomerId = 2,
+                    EmployeeId = 2,
+                    HubId = 2,
+                    ServiceId = 2,
+                }
+            );
         }
 
     }
---
+++ src/Services/SSTHub/SSTHub.Infrastucture/Migrations/20240116132246_SeedData.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SSTHub.Infrastucture.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "CreatedAt", "Email", "FirstName", "LastName", "Phone" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7581), "testRA@test.com", "Roman", "Aaaa", "1111111111" },
+                    { 2, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7599), "testIB@test.com", "Ivan", "BBBB", "2222222222" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hubs",
+                columns: new[] { "Id", "CreatedAt", "IsActive", "Name" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(6576), true, "TestHub1" },
+                    { 2, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(6709), true, "TestHub2" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Services",
+                columns: new[] { "Id", "CreatedAt", "DurationInMinutes", "IsActive", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7384), 60, true, "TestService1", 100 },
+                    { 2, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7401), 60, true, "TestService2", 100 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "CreatedAt", "Email", "FirstName", "HubId", "IsActive", "LastName", "Phone", "Position" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7670), "testDW@test.com", "Dmytro", 1, true, "Watson", "1231231231", (byte)0 },
+                    { 2, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7687), "testPA@test.com", "Petro", 2, true, "Abc", "43434343433", (byte)1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "CreatedAt", "CustomerId", "EmployeeId", "HubId", "IsActive", "ServiceId", "StartAt", "Status" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7748), 1, 1, 1, true, 1, new DateTime(2024, 1, 17, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7755), (byte)0 },
+                    { 2, new DateTime(2024, 1, 16, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7781), 2, 2, 2, true, 2, new DateTime(2024, 1, 15, 15, 22, 46, 189, DateTimeKind.Local).AddTicks(7788), (byte)1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Services",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hubs",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
